b2bd5c5e7dd9277da105ce8153747a3bd5b45adc
Mohammad Aryo Senoaji <aryosenoaji877@gmail.com>
2023-10-12 22:45:50 +0700

    meng update ai

--- Assets/Scripts/Player/NpcMovement.cs
+++ Assets/Scripts/Player/NpcMovement.cs
@@ -13,21 +13,25 @@ public class NpcMovement : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         patrolPoints = new Vector2[4];
-        patrolPoints[0] = transform.position + Vector3.up; // up
-        patrolPoints[1] = transform.position + Vector3.down; // down
-        patrolPoints[2] = transform.position + Vector3.left; // left
-        patrolPoints[3] = transform.position + Vector3.right; // right
+
+        // make point for patrol
+        patrolPoints[0] = new Vector2(transform.position.x, transform.position.y + 2.0f); // up
+        patrolPoints[1] = new Vector2(transform.position.x, transform.position.y - 2.0f); // down
+        patrolPoints[2] = new Vector2(transform.position.x - 2.0f, transform.position.y); // left
+        patrolPoints[3] = new Vector2(transform.position.x + 2.0f, transform.position.y); // right
     }
 
     void Update()
     {
         Patrol();
+        UpdateAnimator();
     }
 
     void Patrol()
     {
         Vector2 targetPosition = patrolPoints[currentPatrolIndex];
 
+        // move to the next patrol point
         if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
         {
             currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
@@ -36,11 +40,11 @@ public class NpcMovement : MonoBehaviour
 
         Vector2 moveDirection = (targetPosition - (Vector2)transform.position).normalized;
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
-        UpdateAnimator(moveDirection);
     }
 
-    void UpdateAnimator(Vector2 moveDirection)
+    void UpdateAnimator()
     {
+        Vector2 moveDirection = (patrolPoints[currentPatrolIndex] - (Vector2)transform.position).normalized;
         animator.SetFloat("Horizontal", moveDirection.x);
         animator.SetFloat("Vertical", moveDirection.y);
     }
