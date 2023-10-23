a9f0786cb92af15f1ec64f0ba4411e9566214613
Mohammad Aryo Senoaji <aryosenoaji877@gmail.com>
2023-10-23 21:53:32 +0700

    perubahan kecil
    scripts dan prefabs ada penambahan sedikit

---
+++ Assets/Scripts/UI/Inventory/InventoryPage.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPage : MonoBehaviour
+{
+    [SerializeField]
+    private InventoryItem spicePrefab;
+
+
+    [SerializeField]
+    private RectTransform contentPanel;
+}
