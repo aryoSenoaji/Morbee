3b4bf3b7e791f2f6e8e4d1820bea0d44da29d761
Mohammad Aryo Senoaji <aryosenoaji877@gmail.com>
2023-11-08 21:13:49 +0700

    menyelesaikan inventry system

--- Assets/Scripts/UI/InventoryBooks/BookItemUI.cs
+++ Assets/Scripts/UI/InventoryBooks/BookItemUI.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using static UnityEditor.Progress;
 using UnityEngine.UI;
 
 public class BookItemUI : MonoBehaviour
 {
     [SerializeField] private Image itemImage;
+    [SerializeField] private TMP_Text itemDescription;
 
     public void UpdateUI(BookItem item)
     {
         itemImage.sprite = item.spiceImage;
+        itemDescription.text = item.description;
     }
 }
