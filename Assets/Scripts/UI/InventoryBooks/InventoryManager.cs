335785b45bd0fa55e3245122232445bdd92c5c66
Mohammad Aryo Senoaji <aryosenoaji877@gmail.com>
2023-11-10 17:02:08 +0700

    salah

---
+++ Assets/Scripts/UI/InventoryBooks/InventoryManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    //// Buat delegate untuk event
+    //public delegate void OnInventoryToggle();
+    //public static event OnInventoryToggle InventoryToggled;
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.I))
+    //    {
+    //        // Panggil event InventoryToggled
+    //        InventoryToggled?.Invoke();
+    //    }
+    //}
+}
