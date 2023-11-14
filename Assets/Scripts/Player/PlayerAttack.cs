594a15ab16dced4c57e53e8c6c8df57bd7a18add
rizwarnugroho <rizwarnugroho@gmail.com>
2023-11-14 07:50:22 +0700

    menambahkan animasi
    - menambahkan ahoole movement
    - menambahkan animasi attack player
    - masih ada bug di player ketika di klik untuk attack

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -9,28 +10,48 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject[] fireballs;
 
-    private Animator anim;
-    private PlayerController playerController;
-    private float cooldownTimer = Mathf.Infinity;
+    public Animator anim;
+    /*private PlayerController playerController;
+    private float cooldownTimer = Mathf.Infinity;*/
+
+    public Rigidbody2D rb;
+    private float attackTime = 0.25f;
+    private float attackCounter = 0.25f;
+    private bool isAttacking;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
-        playerController = GetComponent<PlayerController>();
+        /*playerController = GetComponent<PlayerController>();*/
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerController)
+        if (isAttacking)
+        {
+            rb.velocity = Vector2.zero;
+            attackCounter -= Time.deltaTime;
+            if (attackCounter <= 0)
+            {
+                anim.SetBool("isAttacking", false);
+                isAttacking = false;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            attackCounter = attackTime;
+            anim.SetBool("isAttacking", true);
+            isAttacking = true;
+        }
+        /*if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerController)
             Attack();
 
-        cooldownTimer += Time.deltaTime; 
+        cooldownTimer += Time.deltaTime;*/
     }
 
     private void Attack()
     {
-        anim.SetTrigger("attack");
-        cooldownTimer = 0;
-
+        
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,77 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 2f;
 
-    private Vector2 movement;
-    private Rigidbody2D rb;
-    private Animator animator;
+    public float moveSpeed = 5f;
 
-    private void Awake()
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-    }
-
-    private void OnMove(InputValue value)
-    {
-        movement = value.Get<Vector2>();
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        if (movement.x == 1 || movement.x == -1 || movement.y == 1 || movement.y == -1)
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
-            animator.SetFloat("LastMoveHorizontal", movement.x);
-            animator.SetFloat("LastMoveVertical", movement.y);
+            animator.SetFloat("LastMoveHorizontal", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("LastMoveVertical", Input.GetAxisRaw("Vertical"));
         }
+
+       
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-
 }
-
-
-// in this below script before using the movement new input system key binding
-//public class PlayerController : MonoBehaviour
-//{
-//    public float moveSpeed = 2f;
-
-//    public Rigidbody2D rb;
-//    public Animator animator;
-
-//    Vector2 movement;
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        movement.x = InputSystem.GetDevice<Keyboard>().dKey.ReadValue() - InputSystem.GetDevice<Keyboard>().aKey.ReadValue();
-//        movement.y = InputSystem.GetDevice<Keyboard>().wKey.ReadValue() - InputSystem.GetDevice<Keyboard>().sKey.ReadValue();
-
-//        animator.SetFloat("Horizontal", movement.x);
-//        animator.SetFloat("Vertical", movement.y);
-//        animator.SetFloat("Speed", movement.sqrMagnitude);
-
-//        Debug.Log("Movement Vector: " + movement);
-
-//        if (movement.x == 1 || movement.x == -1 || movement.y == 1 || movement.y == -1)
-//        {
-//            animator.SetFloat("LastMoveHorizontal", movement.x);
-//            animator.SetFloat("LastMoveVertical", movement.y);
-//        }
-//    }
-
-//    void FixedUpdate()
-//    {
-//        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement);
-//    }
-//}
