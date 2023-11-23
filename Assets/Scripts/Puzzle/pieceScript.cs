10e0644b36d8ccfb498b18f185bfbcd9b6f74e5a
Mohammad Aryo Senoaji <aryosenoaji877@gmail.com>
2023-11-23 11:44:37 +0700

    memperbaiki PieceScript

--- Assets/Scripts/Puzzle/pieceScript.cs
+++ Assets/Scripts/Puzzle/pieceScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
@@ -10,34 +8,46 @@ public class PieceScript : MonoBehaviour
     public bool InTruePosition;
     public bool Selected;
     public bool AllPieceInCurrentPosition;
-    // Start is called before the first frame update
+
     void Start()
     {
         randomPosition = transform.position;
-        transform.position= new Vector3(Random.Range(2f,8f), Random.Range(4.2f,-3f));
+        transform.position = new Vector3(Random.Range(2f, 8f), Random.Range(4.2f, -3f));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(AllPieceInCurrentPosition == false)
+        if (AllPieceInCurrentPosition == false)
         {
-            if (Vector3.Distance(transform.position, randomPosition)< 0.5f)
+            if (Vector3.Distance(transform.position, randomPosition) < 0.5f)
             {
                 if (!Selected)
                 {
-                    if(InTruePosition == false)
+                    if (InTruePosition == false)
                     {
                         transform.position = randomPosition;
                         InTruePosition = true;
                         GetComponent<SortingGroup>().sortingOrder = 0;
+                        CheckAllPiecesInPosition(); // Check if all pieces are in the correct position
                     }
                 }
-            }  
+            }
         }
-        if(InTruePosition == true)
+    }
+
+    void CheckAllPiecesInPosition()
+    {
+        // Check if all pieces are in the correct position
+        GameObject[] puzzlePieces = GameObject.FindGameObjectsWithTag("Puzzle");
+        foreach (GameObject piece in puzzlePieces)
         {
-            SceneManager.LoadScene("MapMain");
+            if (!piece.GetComponent<PieceScript>().InTruePosition)
+            {
+                return; // If any piece is not in the correct position, exit the method
+            }
         }
+
+        // If all pieces are in the correct position, reload the scene
+        SceneManager.LoadScene("MapMain");
     }
 }
